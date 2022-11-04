6b6b0024f7843404bd8ec844cd99b76c9fdc75cd
hackedsdq <hackedsdq@gmail.com>
2022-11-04 15:02:27 +0100

    added-packages

--- resources/js/components/Opportunities.jsx
+++ resources/js/components/Opportunities.jsx
@@ -1,42 +1,14 @@
 import React,{useState,useEffect} from 'react'
 import Header from './static_components/Header'
 import SideBar from './static_components/SideBar'
-import {Box,Modal,Button} from '@mui/material'
-import {DataGrid} from '@mui/x-data-grid'
 import PageTitle from './static_components/PageTitle'
-import EditModal from './static_components/EditModal'
-import UserActions from './user_actions/UserActions'
-import DataGridTable from './static_components/DataGridTable'
+import DndBoard from './static_components/DndBoard'
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './dnd_board_resources/config/theme';
 
 export default function Opportunities() {
 
   let title = "Opportunités"
-
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    { field: 'delete', headerName: 'Delete', width: 70, renderCell:(params)=> <UserActions user={params.row} action="delete"/>  },
-    { field: 'modify', headerName: 'Modify', width: 70, renderCell:(params)=> <UserActions user={params.row} action="modify"/> },
-  ];
-  
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
 
   //const [open,setOpen]=useState(false);
 
@@ -66,11 +38,10 @@ export default function Opportunities() {
               {/* start page title */}
               <PageTitle title={title} />
               {/* end page title */} 
-
-              {/* ------------------------ edit modal ------------------ */}
-              <EditModal />
-               {/* ------------------------ show datagrid table search ------------------ */}
-              <DataGridTable title={title} columns={columns} rows={rows}  />
+              {/* ------------------------ drag and drop board ------------------ */}
+              <ChakraProvider  resetCSS={false} theme={theme} >
+                  <DndBoard />
+              </ChakraProvider>
               {/* end row */}
             </div> {/* container */}
           </div> {/* content */}

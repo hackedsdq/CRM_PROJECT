126c43db1ed42705183ff9717f81d4e6ed6fe24c
nihelBelhadjKacem <nihelbelhadjkacem13@gmail.com>
2022-11-06 23:10:54 +0100

    creation du controlleur /model/migrayion prospects

---
+++ database/migrations/2022_11_06_220433_create_prospects_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prospects', function (Blueprint $table) {
+            $table->id();
+            $table->nom();
+            $table->prenom();
+            $table->société();
+            $table->fonction();
+            $table->email();
+            $table->téléphone();
+            $table->adresse();
+            $table->site_web();
+            $table->statut();
+            $table->source();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prospects');
+    }
+};

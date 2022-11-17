a2fcb9b8a1ecb650d5cfa73348e0f0fb8ba4f2e8
soundoussben <soundoussbenba@gmail.com>
2022-11-17 20:56:09 +0100

    EditModalShowProspects-add

---
+++ resources/js/components/static_components/EditModalShowProspects.jsx
@@ -0,0 +1,52 @@
+
+import React from 'react'
+
+export default function EditModalShowProspects(props) {
+  return (
+<div>
+<div class="modal fade" id="full-width-modal"  tabindex="-1" role="dialog" aria-labelledby="fullWidthModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-full-width">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="fullWidthModalLabel">Details Prospect</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
+            </div>
+            <div>
+                <table center>
+                    <tr center>
+                    <td><b>Last Name:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>First Name:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>Email:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>Fonction:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>Telephone:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>Adress:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>Status:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>Source:</b></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div></div>
+  )
+}

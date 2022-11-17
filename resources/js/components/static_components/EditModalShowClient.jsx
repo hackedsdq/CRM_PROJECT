c86c50f68d8b68adfb83b02a9ff60be448fb0a24
soundoussben <soundoussbenba@gmail.com>
2022-11-17 21:42:15 +0100

    EditModalShowContacts-Modified

--- resources/js/components/static_components/EditModalShowClient.jsx
+++ resources/js/components/static_components/EditModalShowClient.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 export default function EditModalShowClient(props) {
   return (
 <div>
-<div class="modal fade" id="full-width-modal"  tabindex="-1" role="dialog" aria-labelledby="fullWidthModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-full-width">
+<button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#bs-example-modal-lg">Large Modal</button>
+<div class="modal fade" id="bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="fullWidthModalLabel">Details Client</h4>
+                <h4 class="modal-title" id="myLargeModalLabel">Details Client</h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
             </div>
             <div>
@@ -30,9 +31,7 @@ export default function EditModalShowClient(props) {
                    
                 </table>
             </div>
-            <div class="modal-body">
-                ...
-            </div>
+        
             <div class="modal-footer">
                 <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
             </div>
--- resources/js/components/static_components/EditModalShowProspects.jsx
+++ resources/js/components/static_components/EditModalShowProspects.jsx
@@ -4,13 +4,15 @@ import React from 'react'
 export default function EditModalShowProspects(props) {
   return (
 <div>
-<div class="modal fade" id="full-width-modal"  tabindex="-1" role="dialog" aria-labelledby="fullWidthModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-full-width">
+<button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#bs-example-modal-lg">Large Modal</button>
+<div class="modal fade" id="bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="fullWidthModalLabel">Details Prospect</h4>
+                <h4 class="modal-title" id="myLargeModalLabel">Details Prospects</h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
             </div>
+     
             <div>
                 <table center>
                     <tr center>
@@ -39,9 +41,7 @@ export default function EditModalShowProspects(props) {
                     </tr>
                 </table>
             </div>
-            <div class="modal-body">
-                ...
-            </div>
+         
             <div class="modal-footer">
                 <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
             </div>

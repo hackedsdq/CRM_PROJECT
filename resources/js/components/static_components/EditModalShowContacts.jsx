c26a7b77d9d2b5c2fb77aa7577fa134283b938d3
soundoussben <soundoussbenba@gmail.com>
2022-11-17 20:38:43 +0100

    editModalShowContacts-add

--- resources/js/components/static_components/EditModalShowContacts.jsx
+++ resources/js/components/static_components/EditModalShowContacts.jsx
@@ -11,12 +11,33 @@ export default function EditModalShow(props) {
                 <h4 class="modal-title" id="fullWidthModalLabel">Details Contact</h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
             </div>
+            <div>
+                <table center>
+                    <tr center>
+                    <td><b>Last Name:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>First Name:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>Email:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>Fonction:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>Telephone:</b></td>
+                    </tr>
+                    <tr>
+                    <td><b>Client:</b></td>
+                    </tr>
+                </table>
+            </div>
             <div class="modal-body">
                 ...
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
     </div>

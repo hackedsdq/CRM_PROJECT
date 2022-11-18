d5f5ba5affb94c8c6be81b72a94146d513d85cfb
hackedsdq <hackedsdq@gmail.com>
2022-11-18 14:12:39 +0100

    contact-model-modified

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,19 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Contact extends Model
+class Contact extends Authenticatable
 {
-    use HasFactory;protected $fillable = [  'nom',
+    use HasApiTokens, HasFactory, Notifiable;
+    
+    protected $fillable = [
+    'nom',
     'prenom',
     'email',
     'password',
     'fonction',
     'telephone'
-    
-];
-<<<<<<< HEAD
+    ];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
 public function client(){
 
     return $this->hasMany(Client::class);
@@ -24,7 +46,4 @@ public function user(){
 
     return $this->belongsToMany(User::class);
 }
-=======
-
->>>>>>> d44b412accdd5d319bda7004b61196fe5c2821c0
 }

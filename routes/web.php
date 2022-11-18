c7b65da4dde7a3ea4e73229b7fdd3ddacaeb3653
hackedsdq <hackedsdq@gmail.com>
2022-11-18 14:23:53 +0100

    routes-modified

--- routes/web.php
+++ routes/web.php
@@ -17,26 +17,45 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-Route::get('/',[\App\Http\Controllers\HomeController::class, 'index']);
-Route::get('/signin',[\App\Http\Controllers\SigninController::class, 'index']);
-Route::get('/signup',[\App\Http\Controllers\HomeController::class, 'signup']);
-Route::get('/prospects',[\App\Http\Controllers\ProspectController::class, 'index']);
-Route::get('/clients',[\App\Http\Controllers\ClientController::class, 'index']);
-Route::get('/opportunities',[\App\Http\Controllers\OpportunitiesController::class, 'index']);
 
-Route::get('/contacts',[\App\Http\Controllers\ContactController::class, 'index']);
-Route::get('/users',[\App\Http\Controllers\HomeController::class, 'users']);
-Route::get('/calendar',[\App\Http\Controllers\CalendarController::class, 'index']);
-Route::get('/SigninCostumer',[\App\Http\Controllers\SiginCostumerController::class, 'index']);
-Route::get('/Profile',[\App\Http\Controllers\HomeController::class, 'Profile']);
-Route::get('/Produits',[\App\Http\Controllers\ProduitController::class, 'index']);
+
+// guest routes
+Route::get('/signin',[\App\Http\Controllers\SigninController::class, 'index'])->name('guest.signin');
+Route::get('/SigninCostumer',[\App\Http\Controllers\SiginCostumerController::class, 'index'])->name('guest.signincostumer');
+// end of guest routes
 
 
 
-Route::get('adcom/', [AdminCommercialAuthController::class, 'index'])->name('adcom.home');
-Route::get('adcom/login', [AdminCommercialAuthController::class, 'login'])->name('adcom.login');
-Route::post('adcom/login', [AdminCommercialAuthController::class, 'handleLogin'])->name('adcom.handleLogin');
-Route::get('adcom/logout', [AdminCommercialAuthController::class, 'index'])->name('adcom.logout');
+
+// contact routes
+Route::get('login/', [\App\Http\Controllers\ContactsAuthController::class, 'login'])->name('contacts.login');
+Route::post('login/', [\App\Http\Controllers\ContactsAuthController::class, 'handleLogin'])->name('contacts.handleLogin');
+Route::get('logout/', [\App\Http\Controllers\ContactsAuthController::class, 'index'])->name('contacts.logout');
+
+Route::get('opportunities/',[\App\Http\Controllers\OpportunitiesController::class, 'index'])->name('contacts.opportunities');
+Route::get('calendar/',[\App\Http\Controllers\CalendarController::class, 'index'])->name('contacts.calendar');
+Route::get('Profile/',[\App\Http\Controllers\ContactsAuthController::class, 'index'])->name('contacts.profile');
+// end of contacts route
+
+
+
+// admins and commercials routes
+Route::get('adcom/', [\App\Http\Controllers\AdminCommercialAuthController::class, 'index'])->name('adcom.home');
+Route::get('adcom/login', [\App\Http\Controllers\AdminCommercialAuthController::class, 'login'])->name('adcom.login');
+Route::post('adcom/login', [\App\Http\Controllers\AdminCommercialAuthController::class, 'handleLogin'])->name('adcom.handleLogin');
+Route::get('adcom/logout', [\App\Http\Controllers\AdminCommercialAuthController::class, 'index'])->name('adcom.logout');
+
+
+Route::get('adcom/prospects',[\App\Http\Controllers\ProspectController::class, 'index'])->name('adcom.prospects');
+Route::get('adcom/clients',[\App\Http\Controllers\ClientController::class, 'index'])->name('adcom.clients');
+Route::get('adcom/opportunities',[\App\Http\Controllers\OpportunitiesController::class, 'index'])->name('adcom.opportunities');
+Route::get('adcom/contacts',[\App\Http\Controllers\ContactController::class, 'index'])->name('adcom.contacts');
+Route::get('adcom/users',[\App\Http\Controllers\HomeController::class, 'users'])->name('adcom.users');
+Route::get('adcom/calendar',[\App\Http\Controllers\CalendarController::class, 'index'])->name('adcom.calendar');
+Route::get('adcom/Produits',[\App\Http\Controllers\ProduitController::class, 'index'])->name('adcom.produits');
+// end of admins and commercials routes
+
 
 // auth middlewares
-Route::get('adcom/', [AdminCommercialAuthController::class, 'index'])->name('adcom.home')->middleware('auth:webadcom');
+Route::get('adcom/', [\App\Http\Controllers\AdminCommercialAuthController::class, 'index'])->name('adcom.home')->middleware('auth:webadcom');
+Route::get('/', [\App\Http\Controllers\ContactsAuthController::class, 'index'])->middleware('auth:web');
